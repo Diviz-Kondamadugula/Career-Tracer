7d8cb3dd4b2569e82de7a19c4590df347f581ca1
Diviz Kondamadugula <kondamaduguladiviz@gmail.com>
2022-06-26 21:26:22 +0530

    first commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            For any student/Fresher that are applying for the job ,
+            They can track the applications which are declined,pending and in interview stage.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
